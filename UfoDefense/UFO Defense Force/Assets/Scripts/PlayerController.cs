b2dd46f8271acb1b0203cd3d7a13f1dc35eb3c3d
Brandon <github@briley.me>
2023-02-11 23:57:02 -0700

    UFO Defender Game
    Created a UFO defender game, made it so player can move left and right, out of bounds objects get destroyed

---
+++ UfoDefense/UFO Defense Force/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed;
+    public float xRange;
+
+    public Transform blaster;
+
+    public GameObject lazerBolt;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    // Set's Horizontal input to recieve values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+
+    // Moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+    //Keeps player within bounds
+    //left side wall
+        if(transform.position.x < -xRange )
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    //Right side wall
+        if(transform.position.x > xRange )
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //If spacebar is pressed fire lazerbolt
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Create Lazerbolt at the blaster transform position maintaining the objects rotation.
+            Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
