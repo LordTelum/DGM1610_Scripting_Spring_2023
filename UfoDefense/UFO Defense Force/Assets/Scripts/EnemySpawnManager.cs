0f593fcc65f65b45ac4f4e52d3b6668ef38c01c4
Brandon <github@briley.me>
2023-02-20 22:31:51 -0700

    Enemy code

---
+++ UfoDefense/UFO Defense Force/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject[] ufoPrefabs; // Array to store UFO ships
+    private float spawnRangeX = 20f;
+    private float spawnPosZ = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0,spawnPosZ);
+            int ufoIndex = Random.Range(0,ufoPrefabs.Length); //Picks a random UFO from the array
+            Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation); //spawns a indexed UFO from the array 
+        }
+    }
+}
